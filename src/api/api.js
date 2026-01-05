e088091dc8b8ea2df015d387de005381bf8cf979
Karthan Venkatesh <karthanvenkateshvenkatesh@gmail.com>
2026-01-05 19:35:37 +0530

    updated api

--- src/api/api.js
+++ src/api/api.js
@@ -2,9 +2,10 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api",
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000",
   withCredentials: true,
 });
+
 
 api.interceptors.request.use((config) => {
   const token = Cookies.get("jwt_token");
