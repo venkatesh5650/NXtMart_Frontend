8143edb232350181350806882ad7dab03157c362
Karthan Venkatesh <karthanvenkateshvenkatesh@gmail.com>
2026-01-05 19:17:37 +0530

    issue resolving of api

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000",
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api",
   withCredentials: true,
 });
 
